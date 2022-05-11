c27ca8366ef15346b75faf0fb19b49f73dc40637
mapaxxxe <65698934+mapaxxxe@users.noreply.github.com>
2022-05-11 04:48:56 -0400

    1.0
    primera version del juego completada

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,33 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int score;
+    public Text Scoretext;
+    public GameObject Playbuttom;
+    public GameObject gameover;
+    public GameObject space;
+    public GameObject Retry;
+    public GameObject troncos;
+    public player player;
+    public AudioSource boton;
+    public GameObject exit;
+    public GameObject tittle;
+
+    private void Awake()
     {
-        
+        Application.targetFrameRate = 60;
+        space.SetActive(true);
+        gameover.SetActive(false);
+        Retry.SetActive(false);
+        exit.SetActive(false);
+        tittle.SetActive(true);
+        Pause();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
         
+        score = 0;
+        Scoretext.text = score.ToString();
+        gameover.SetActive(false);
+        Playbuttom.SetActive(false);
+        space.SetActive(false);
+        Retry.SetActive(false);
+        exit.SetActive(false);
+        tittle.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        troncos[] Troncos = FindObjectsOfType<troncos>();
+
+        for (int i = 0; i < Troncos.Length; i++)
+        {
+            Destroy(Troncos[i].gameObject);
+        }
+
+        boton.Play();
+    }
+
+
+    public void playboton()
+    {
+        boton.Play();
+    }
+    
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
     }
 
 
     public void Gameover()
     {
-
+        gameover.SetActive(true);
+        Retry.SetActive(true);
+        exit.SetActive(true);
+        Pause();
     }
 
-    public void WinGame()
+
+    public void ResetButton()
     {
-
+        SceneManager.LoadScene("gameplay");
     }
 
 
 
+
+    public void increasscore()
+    {
+        score++;
+        Scoretext.text = score.ToString();
+
+    }
+
+    public void exitgame()
+    {
+        Application.Quit();
+    }
 
 }
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -8,6 +8,9 @@ public class player : MonoBehaviour
     public float gravedad = -9.8f;
     public float fuerza = 5f;
     private SpriteRenderer spriteRenderer;
+    public AudioSource jump;
+    public AudioSource score;
+    public AudioSource crash;
 
 
     private void Awake()
@@ -15,16 +18,42 @@ public class player : MonoBehaviour
         
     }
 
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0;
+        transform.position = position;
+        direction = Vector3.zero;
+        
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             direction = Vector3.up * fuerza;
+            jump.Play();
         }
 
         direction.y += gravedad * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "obstaculo")
+        {
+            FindObjectOfType<GameManager>().Gameover();
+            crash.Play();
+        }
+
+        else if(collision.gameObject.tag == "score")
+        {
+            FindObjectOfType<GameManager>().increasscore();
+            score.Play();
+
+        }
+    }
+
 
 }
